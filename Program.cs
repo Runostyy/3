1659f6d58933bf0016213e626864f4f07e2719b4
Runos <Runos@RUNOSTY>
2024-12-12 23:05:38 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Оголошуємо лямбда-вираз з використанням делегату Predicate
+        Predicate<int> isPositive = x => x > 0;
+
+        // Введення числа з консолі
+        Console.WriteLine("Введіть число:");
+        int number = Convert.ToInt32(Console.ReadLine());
+
+        // Викликаємо лямбда-вираз та виводимо результат
+        if (isPositive(number))
+        {
+            Console.WriteLine($"{number} є додатнім числом.");
+        }
+        else
+        {
+            Console.WriteLine($"{number} не є додатнім числом.");
+        }
+    }
+}
